797c6ae07db784cdf2890b513b92e9ac48470df0
Timmons C. Player <timmons.player@spirent.com>
2015-04-30 11:03:08 -0400

    D'oh.  Create some wd device nodes so we can actually use an ATA disk

--- sys/rump/dev/lib/libata/ata_component.c
+++ sys/rump/dev/lib/libata/ata_component.c
@@ -4,14 +4,28 @@
 #include <sys/conf.h>
 #include <sys/device.h>
 #include <sys/bus.h>
-
-#include "rump_private.h"
+#include <sys/stat.h>
 
 #include "ioconf.c"
 
+#include "rump_private.h"
+#include "rump_vfs_private.h"
+
 RUMP_COMPONENT(RUMP_COMPONENT_DEV)
 {
+    extern struct bdevsw wd_bdevsw;
+    extern struct cdevsw wd_cdevsw;
+    devmajor_t bmaj, cmaj;
+
     config_init_component(cfdriver_ioconf_ata,
                           cfattach_ioconf_ata,
                           cfdata_ioconf_ata);
+
+    bmaj = cmaj = -1;
+    FLAWLESSCALL(devsw_attach("wd", &wd_bdevsw, &bmaj, &wd_cdevsw, &cmaj));
+
+    FLAWLESSCALL(rump_vfs_makedevnodes(S_IFBLK, "/dev/wd0", 'a',
+                                       bmaj, 0, 8));
+    FLAWLESSCALL(rump_vfs_makedevnodes(S_IFCHR, "/dev/rwd0", 'a',
+                                       cmaj, 0, 8));
 }
